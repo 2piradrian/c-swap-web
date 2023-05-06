fa85e4099f62dd54b3abcca7f009e455f5ca8b8e
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-05-06 20:33:31 -0300

    header & navbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Navbar from "../navbar/Navbar";
 import Image from "next/image";
 import homeicon from "@/assets/icons/bxs-home.svg";
+import menuicon from "@/assets/icons/bxs-menu.svg";
 import style from "./style.module.css";
 
 export default function Header() {
@@ -10,10 +11,6 @@ export default function Header() {
 
 	useEffect(() => {
 		setClosed(window.innerWidth < 900);
-		window.addEventListener("resize", () => setClosed(window.innerWidth < 900), false);
-	}, []);
-
-	useEffect(() => {
 		window.addEventListener("resize", () => setClosed(window.innerWidth < 900), false);
 	}, []);
 
@@ -26,8 +23,13 @@ export default function Header() {
 						<span>Home</span>
 					</div>
 				</Link>
-				{/* BURGER MENU ICON */}
-				{!closed && <Navbar setClosed={setClosed} />}
+				<Image
+					src={menuicon}
+					alt="burger icon"
+					className={style.burger}
+					onClick={() => setClosed(!closed)}
+				/>
+				<Navbar setClosed={setClosed} closed={closed} />
 			</div>
 		</header>
 	);
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
+import cross from "@/assets/icons/bxs-cross.svg";
 import style from "./style.module.css";
 
 type Props = {
 	setClosed: Function;
+	closed: boolean | undefined;
 };
 
-export default function Navbar({ setClosed }: Props) {
+export default function Navbar({ setClosed, closed }: Props) {
 	return (
 		<div
-			className={style.navBox}
+			className={` ${style.navBox} ${!closed ? style.navBoxActive : style.navBoxInactive}`}
 			onClick={() => {
 				if (window.innerWidth < 900) setClosed(true);
 			}}>
-			{/* CLOSE ICON */}
+			<Image src={cross} alt="burger icon" className={style.close} />
 			<ul className={style.navList}>
 				<li className={style.navRoutes}>
 					<Link href="/home#services" aria-label="Servicios">
