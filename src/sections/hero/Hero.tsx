7628b571c7cd7bbe0a58db3c3cfb441b94fbade9
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-05-09 17:55:24 -0300

    hero details

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,4 +1,7 @@
 import GradientTitle from "@/components/gradient-title/GradientTitle";
+import heroimage from "../../assets/images/heroimage.svg";
+import Image from "next/image";
+import arrow from "../../assets/icons/arrow.svg";
 import style from "./style.module.css";
 
 export default function Hero() {
@@ -6,6 +9,10 @@ export default function Hero() {
 		<div className="bigcontainer">
 			<GradientTitle title="Creamos productos digitales" />
 			<h2 className={style.subtitle}>Centrados en el usuario y en el negocio</h2>
+			<Image src={heroimage} alt="" className={style.decoration} />
+			<div className={style.arrow}>
+				<Image src={arrow} alt="arrow" />
+			</div>
 		</div>
 	);
 }
