7960befa83198615b6bf8876f0d151e425719688
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-06-11 20:16:20 -0300

    service cards

--- src/components/service-card/ServiceCard.tsx
+++ src/components/service-card/ServiceCard.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import style from "./style.module.css";
+import Image from "next/image";
 
-function ServiceCard() {
-	return <div className={style.container}>a</div>;
+type Props = {
+	service: string;
+	icon: string;
+};
+
+function ServiceCard({ service, icon }: Props) {
+	return (
+		<div className={style.container}>
+			<Image
+				src={require(`../../assets/icons/${icon}.svg`)}
+				alt={service}
+				className={style.icon}
+			/>
+			<h3>{service}</h3>
+		</div>
+	);
 }
 
 export default ServiceCard;
--- src/sections/services/Services.tsx
+++ src/sections/services/Services.tsx
@@ -1,16 +1,16 @@
 import ServiceCard from "@/components/service-card/ServiceCard";
 import Title from "@/components/title/Title";
-import React from "react";
 import style from "./style.module.css";
+import React from "react";
 
 function Services() {
 	return (
 		<section className="betweencontainer">
 			<Title title="Mira todo lo que tenemos para ofrecerte" />
 			<div className={style.serviceContainer}>
-				<ServiceCard />
-				<ServiceCard />
-				<ServiceCard />
+				<ServiceCard service="Diseño de identidad" icon="design-service" />
+				<ServiceCard service="Programación web" icon="develop-service" />
+				<ServiceCard service="Diseño UX/UI" icon="uxui-service" />
 			</div>
 			<div>Contratame</div>
 		</section>
